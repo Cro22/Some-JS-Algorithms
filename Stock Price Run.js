d5dab9c87701c1f955a2403d2c46fbf4e8991d8b
Jesus <jesus.nunez2050@gmail.com>
2020-05-07 10:54:11 -0400

    Stock Price Run.js Check

--- Stock Price Run.js
+++ Stock Price Run.js
@@ -1,11 +1,18 @@
 function stock_runs(prices) {
     let count = 0;
+    let max = -Infinity;
     for (let i = 0; i < prices.length; i++) {
-        if (prices[i] !== prices[i + 1]&& i !== 0 && i !== prices.length) {
+        if (prices[i] !== prices[i + 1]) {
             count += 1;
+        }
+        if (prices[i] === prices[i + 1]) {
+            count = 0;
+        }
+        if (count > max) {
+            max = count;
         }
     }
     return count;
 }
 
-console.log(stock_runs([2, 3, 4, 3, 2, 1]))
+console.log(stock_runs([1,2,3]))
